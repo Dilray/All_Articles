1f54d723629a1c2a1139dea88777b2dd826b907f
Kostya <114941602+pedevyat@users.noreply.github.com>
2024-11-19 20:31:50 +0300

    Revert "add new few dependencies"
    This reverts commit 7637909f408526d5289f885f7b7c615e75dbb7e9.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,3 @@
 // Entry point for the build script in your package.json
-import { Turbo } from "@hotwired/turbo-rails"
-import { Application } from "@hotwired/stimulus"
-
-const application = Application.start()
+import "@hotwired/turbo-rails"
+import "./controllers"
