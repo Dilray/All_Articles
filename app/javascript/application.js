7637909f408526d5289f885f7b7c615e75dbb7e9
Kostya <114941602+pedevyat@users.noreply.github.com>
2024-11-19 17:49:24 +0300

    add new few dependencies

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import { Turbo } from "@hotwired/turbo-rails"
+import { Application } from "@hotwired/stimulus"
+
+const application = Application.start()
