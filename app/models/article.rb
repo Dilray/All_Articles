29341ba4c5f72f55c43c787adbc0861a2372f290
PROKLYATbE <spasibo1911@yandex.ru>
2024-11-24 16:39:29 +0300

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,15 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :ratings
 
+  after_create :create_initial_rating
+
+  private def create_initial_rating
+    @user = user
+    Rating.create(article_id: self.id, user_id: @user.full_name, score: 0) if user.present?
+  end
   def average_rating
-    return 0 if ratings.empty?
-    ratings.average(:rating).round(2)
+    @ratings = ratings.where.not(score: 0)
+    return 0 if @ratings.empty?
+    @ratings.average(:score).round(2)
   end
 end
