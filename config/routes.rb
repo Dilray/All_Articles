87d95a763ee9b1abfb4478692af47e7ef7106101
Dilray <115149242+Dilray@users.noreply.github.com>
2024-12-07 19:34:12 +0300

    Quick updates

--- config/routes.rb
+++ config/routes.rb
@@ -8,27 +8,19 @@ Rails.application.routes.draw do
       post 'rate'
     end
   end
+  
   root "articles#index"
-  # get 'articles/index' => 'articles#index'
-  # get 'articles/show' => 'articles#show'
-  # get 'articles/new' => 'articles#new'
-  # get 'articles/create' => 'articles#create'
-  # get 'articles/edit' => 'articles#edit'
-  # get 'articles/update' => 'articles#update'
-  get 'articles/my_articles' => "articles#my_articles"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  # Аутентификация
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', to: redirect('/')
   get '/auth/google_oauth2', to: 'sessions#google_auth'
-  # get '/google/auth', to: 'sessions#google_auth'
   delete '/logout', to: 'sessions#destroy'
+
+  # Devise для пользователей
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-
-
 end
