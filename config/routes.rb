3c9bb4a67089ad786998b629269343ef3b4a09b6
Kostya <114941602+pedevyat@users.noreply.github.com>
2024-12-21 23:35:06 +0300

    Privacy Policy

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
       post :search
     end
     member do
-      post 'rate'
+      post "rate"
     end
   end
-  
+
   root "articles#index"
 
   # get 'articles/index' => 'articles#index'
@@ -17,27 +17,29 @@ Rails.application.routes.draw do
   # get 'articles/create' => 'articles#create'
   # get 'articles/edit' => 'articles#edit'
   # get 'articles/update' => 'articles#update'
-  get 'articles/my_articles' => "articles#my_articles"
-  #get 'articles/topics' => "articles#topics"
-  get 'topics' => 'articles#topics'
-  
-  resources :topics, only: [:new, :create]
+  get "articles/my_articles" => "articles#my_articles"
+  # get 'articles/topics' => "articles#topics"
+  get "topics" => "articles#topics"
+  get "rules" => "articles#rules"
+
+
+  resources :topics, only: [ :new, :create ]
 
   # Аутентификация
-  get '/auth/:provider/callback', to: 'sessions#create'
-  get '/auth/failure', to: redirect('/')
-  get '/auth/google_oauth2', to: 'sessions#google_auth'
-  delete '/logout', to: 'sessions#destroy'
+  get "/auth/:provider/callback", to: "sessions#create"
+  get "/auth/failure", to: redirect("/")
+  get "/auth/google_oauth2", to: "sessions#google_auth"
+  delete "/logout", to: "sessions#destroy"
 
   # Devise для пользователей
   devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks',
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
+    omniauth_callbacks: "users/omniauth_callbacks",
+    sessions: "users/sessions",
+    registrations: "users/registrations"
   }
   resources :users do
     member do
-      get 'profile'
+      get "profile"
     end
-  end 
+  end
 end
